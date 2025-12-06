6f9a5c01dae1055200e06a017881410e217b5022
wungjyan <1170017412@qq.com>
2025-12-06 22:45:35 +0800

    feat: 添加帖子标签常量

--- lib/constants/index.dart
+++ lib/constants/index.dart
@@ -7,3 +7,22 @@ class GlobalConstants {
 class HttpConstants {
   static const String baseUrl = 'https://cnodejs.org/api/v1';
 }
+
+class LabelInfo {
+  final String text;
+  final Color color;
+  const LabelInfo({required this.text, required this.color});
+}
+
+class LabelConstants {
+  static const Map<String, LabelInfo> labels = {
+    'top': LabelInfo(text: '置顶', color: GlobalConstants.primaryColor),
+    'good': LabelInfo(text: '精华', color: Colors.orange),
+    'share': LabelInfo(text: '分享', color: Colors.blue),
+    'ask': LabelInfo(text: '问答', color: Colors.teal),
+  };
+
+  static LabelInfo of(String code) {
+    return labels[code] ?? const LabelInfo(text: '', color: Colors.grey);
+  }
+}
