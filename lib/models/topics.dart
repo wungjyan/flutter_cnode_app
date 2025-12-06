16f38af648da2b658031f525e09b1b051f8f7799
wungjyan <1170017412@qq.com>
2025-12-06 22:47:24 +0800

    feat: 更新类型

--- lib/models/topics.dart
+++ lib/models/topics.dart
@@ -1,4 +1,3 @@
-
 class TopicAuthor {
   final String loginname;
   final String avatarUrl;
@@ -11,12 +10,14 @@ class TopicAuthor {
       avatarUrl: json['avatar_url']?.toString() ?? '',
     );
   }
+
+  factory TopicAuthor.empty() => TopicAuthor(loginname: '', avatarUrl: '');
 }
 
 class TopicItem {
   final String id;
   final String authorId;
-  final String tab;
+  final String? tab;
   final String content;
   final String title;
   final DateTime lastReplyAt;
@@ -30,7 +31,7 @@ class TopicItem {
   TopicItem({
     required this.id,
     required this.authorId,
-    required this.tab,
+    this.tab,
     required this.content,
     required this.title,
     required this.lastReplyAt,
@@ -52,32 +53,123 @@ class TopicItem {
       tab: json['tab']?.toString() ?? '',
       content: json['content']?.toString() ?? '',
       title: json['title']?.toString() ?? '',
-      lastReplyAt: DateTime.tryParse(lastReplyStr) ?? DateTime.fromMillisecondsSinceEpoch(0),
+      lastReplyAt:
+          DateTime.tryParse(lastReplyStr) ??
+          DateTime.fromMillisecondsSinceEpoch(0),
       good: (json['good'] as bool?) ?? false,
       top: (json['top'] as bool?) ?? false,
       replyCount: (json['reply_count'] as num?)?.toInt() ?? 0,
       visitCount: (json['visit_count'] as num?)?.toInt() ?? 0,
-      createAt: DateTime.tryParse(createAtStr) ?? DateTime.fromMillisecondsSinceEpoch(0),
+      createAt:
+          DateTime.tryParse(createAtStr) ??
+          DateTime.fromMillisecondsSinceEpoch(0),
       author: authorJson != null
           ? TopicAuthor.fromJson(authorJson)
           : TopicAuthor(loginname: '', avatarUrl: ''),
     );
   }
 }
-// {
-//       "id": "61f69e4aa08b39f75309c2a8",
-//       "author_id": "4efc278525fa69ac6900000f",
-//       "tab": "share",
-//       "content": "\u003Cdiv class=\"markdown-text\"\u003E\u003Cp\u003E\u003Ca href=\"https://registry.npmmirror.com\"\u003Ehttps://registry.npmmirror.com\u003C/a\u003E npmmirror 镜像站在2013年12月开始就使用基于 koa 的 \u003Ca href=\"https://github.com/cnpm/cnpmjs.org\"\u003Ehttps://github.com/cnpm/cnpmjs.org\u003C/a\u003E 私有 npm 应用搭建，这些年 node 应用框架在快速换代升级，连我们自己造的 egg 都要升级到 TypeScript 了，所以在 2021 年我们启动了 \u003Ca href=\"http://cnpmjs.org\"\u003Ecnpmjs.org\u003C/a\u003E 的技术升级重构，基于 egg 的 TypeScript 模式重新实现 \u003Ca href=\"https://github.com/cnpm/cnpmcore\"\u003Ehttps://github.com/cnpm/cnpmcore\u003C/a\u003E 。\u003C/p\u003E\n\u003Cp\u003Enpm registry 的接口是 100% 实现迁移，然后在数据同步上通过数据库实现足够简单的任务系统，已经在 2022年1月30日完全老数据迁移。\n非常感谢阿里云这么多年来的对中国 npm 镜像云服务器的开源赞助，只能靠仅有的广告位和每年的感谢帖子来回报这份天价的云资源账单。这一次重构之后，我们验证下来至少可以节省一半的云服务器资源，也算是让阿里云的开源赞助可以减轻一些成本负担。\u003C/p\u003E\n\u003Cp\u003E当然 cnpmcore 不仅仅是为了技术升级，我们的核心新能力会跟随 npmfs 黑科技在 2022 年发布出来，到时候 npm install 的安装速度会在 cli 侧和 registry 侧同时发力，让 npm 模块安装速度在可预见的未来达到秒级。\u003C/p\u003E\n\u003Cp\u003EPS：预估你在此期间遇到包数据同步和丢失问题，请先手动同步一次，如果还是没有，请回帖反馈给我们。\u003C/p\u003E\n\u003C/div\u003E",
-//       "title": "npmmirror 镜像站升级公告",
-//       "last_reply_at": "2025-12-05T05:52:38.836Z",
-//       "good": false,
-//       "top": true,
-//       "reply_count": 117,
-//       "visit_count": 221003,
-//       "create_at": "2022-01-30T14:18:50.170Z",
-//       "author": {
-//         "loginname": "fengmk2",
-//         "avatar_url": "https://avatars.githubusercontent.com/u/156269?v=4&s=120"
-//       }
-//     }
+
+class TopicDetail extends TopicItem {
+  final List<ReplyItem> replies;
+
+  TopicDetail({
+    required super.id,
+    required super.authorId,
+    required super.tab,
+    required super.content,
+    required super.title,
+    required super.lastReplyAt,
+    required super.good,
+    required super.top,
+    required super.replyCount,
+    required super.visitCount,
+    required super.createAt,
+    required super.author,
+    required this.replies,
+  });
+
+  factory TopicDetail.fromJson(Map<String, dynamic> json) {
+    final repliesJson = json['replies'] as List<dynamic>?;
+    return TopicDetail(
+      id: json['id']?.toString() ?? '',
+      authorId: json['author_id']?.toString() ?? '',
+      tab: json['tab']?.toString() ?? '',
+      content: json['content']?.toString() ?? '',
+      title: json['title']?.toString() ?? '',
+      lastReplyAt: DateTime.tryParse(json['last_reply_at']?.toString() ?? '') ??
+          DateTime.fromMillisecondsSinceEpoch(0),
+      good: (json['good'] as bool?) ?? false,
+      top: (json['top'] as bool?) ?? false,
+      replyCount: (json['reply_count'] as num?)?.toInt() ?? 0,
+      visitCount: (json['visit_count'] as num?)?.toInt() ?? 0,
+      createAt: DateTime.tryParse(json['create_at']?.toString() ?? '') ??
+          DateTime.fromMillisecondsSinceEpoch(0),
+      author: json['author'] != null
+          ? TopicAuthor.fromJson(json['author'] as Map<String, dynamic>)
+          : TopicAuthor(loginname: '', avatarUrl: ''),
+      replies: repliesJson != null
+          ? repliesJson.map((e) => ReplyItem.fromJson(e as Map<String, dynamic>)).toList()
+          : [],
+    );
+  }
+
+  factory TopicDetail.empty() => TopicDetail(
+        id: '',
+        authorId: '',
+        tab: '',
+        content: '',
+        title: '',
+        lastReplyAt: DateTime.fromMillisecondsSinceEpoch(0),
+        good: false,
+        top: false,
+        replyCount: 0,
+        visitCount: 0,
+        createAt: DateTime.fromMillisecondsSinceEpoch(0),
+        author: TopicAuthor.empty(),
+        replies: const [],
+      );
+}
+
+
+class ReplyItem {
+  final String id;
+  final TopicAuthor author;
+  final String content;
+  final List<String> ups;
+  final DateTime createAt;
+  String replyId;
+  final bool isUped;
+
+  ReplyItem({
+    required this.id,
+    required this.author,
+    required this.content,
+    required this.ups,
+    required this.createAt,
+    required this.replyId,
+    required this.isUped,
+  });
+
+  factory ReplyItem.fromJson(Map<String, dynamic> json) {
+    final createAtStr = json['create_at']?.toString() ?? '';
+    final authorJson = json['author'] as Map<String, dynamic>?;
+    return ReplyItem(
+      id: json['id']?.toString() ?? '',
+      author: authorJson != null
+          ? TopicAuthor.fromJson(authorJson)
+          : TopicAuthor(loginname: '', avatarUrl: ''),
+      content: json['content']?.toString() ?? '',
+      ups:
+          (json['ups'] as List<dynamic>?)
+              ?.map((e) => e?.toString() ?? '')
+              .toList() ??
+          [],
+      createAt:
+          DateTime.tryParse(createAtStr) ??
+          DateTime.fromMillisecondsSinceEpoch(0),
+      replyId: json['reply_id']?.toString() ?? '',
+      isUped: (json['is_uped'] as bool?) ?? false,
+    );
+  }
+}
