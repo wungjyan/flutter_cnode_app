b38a37ccb9bbd731b536420ef918bd01606d4e3d
wungjyan <1170017412@qq.com>
2025-12-06 13:24:31 +0800

    fix: 优化请求封装

--- lib/utils/http.dart
+++ lib/utils/http.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter_cnode_app/constants/index.dart';
 
@@ -24,18 +25,31 @@ class HttpUtils {
           if (response.statusCode! >= 200 && response.statusCode! < 300) {
             return handler.next(response);
           }
-          handler.reject(
+          final code = response.statusCode ?? 0;
+          final statusMsg = response.statusMessage ?? '';
+          final serverMsg = response.data?['error_msg'] ?? '';
+          final msg = serverMsg.isNotEmpty
+              ? serverMsg
+              : 'HTTP $code $statusMsg';
+          handler.next(
             DioException(
               requestOptions: response.requestOptions,
-              message: response.data["msg"] ?? '',
-            ),
+              response: response,
+              type: DioExceptionType.badResponse,
+              message: msg,
+            ) as Response<dynamic>,
           );
         },
         onError: (e, handler) {
-          handler.reject(
+          final serverMsg = e.response?.data?['error_msg'] ?? '';
+          final friendlyMsg = _buildFriendlyMessage(e, serverMsg);
+          handler.next(
             DioException(
               requestOptions: e.requestOptions,
-              message: e.response?.data["msg"] ?? '',
+              response: e.response,
+              type: e.type,
+              error: e.error,
+              message: friendlyMsg,
             ),
           );
         },
@@ -43,11 +57,42 @@ class HttpUtils {
     );
   }
 
+  String _buildFriendlyMessage(DioException e, String serverMsg) {
+    if (serverMsg.isNotEmpty) return serverMsg;
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        return '连接超时';
+      case DioExceptionType.sendTimeout:
+        return '请求发送超时';
+      case DioExceptionType.receiveTimeout:
+        return '响应接收超时';
+      case DioExceptionType.badResponse:
+        final code = e.response?.statusCode;
+        final statusMsg = e.response?.statusMessage ?? '';
+        if (code == 404) return '资源不存在';
+        if (code == 500) return '服务器错误';
+        return 'HTTP ${code ?? ''} $statusMsg';
+      case DioExceptionType.cancel:
+        return '请求已取消';
+      case DioExceptionType.unknown:
+        if (e.error is SocketException) return '网络不可用';
+        return '发生未知错误';
+      default:
+        return '请求失败';
+    }
+  }
+
   get(String url, {Map<String, dynamic>? queryParameters}) async {
     try {
-      Response<dynamic> response = await _dio.get(url, queryParameters: queryParameters);
+      Response<dynamic> response = await _dio.get(
+        url,
+        queryParameters: queryParameters,
+      );
       return response.data;
     } catch (e) {
+      if (e is DioException) {
+        return e.message ?? e.toString();
+      }
       return e.toString();
     }
   }
