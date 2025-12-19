f20950e6022703e264bef87fd3a3d978e3c1b188
wungjyan <1170017412@qq.com>
2025-12-19 17:56:04 +0800

    feat: app 启动时验证登录

--- lib/app_enter.dart
+++ lib/app_enter.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cnode_app/api/user.dart';
 import 'package:flutter_cnode_app/pages/main/index.dart';
 import 'package:flutter_cnode_app/pages/login.dart';
 import 'package:flutter_cnode_app/pages/topic_detail.dart';
+import 'package:flutter_cnode_app/store/token_manage.dart';
+import 'package:flutter_cnode_app/store/user_manage.dart';
 
 class AppEnter extends StatefulWidget {
   const AppEnter({super.key});
@@ -11,6 +14,29 @@ class AppEnter extends StatefulWidget {
 }
 
 class _AppEnterState extends State<AppEnter> {
+  @override
+  void initState() {
+    super.initState();
+    _bootstrapAuth();
+  }
+
+  Future<void> _bootstrapAuth() async {
+    final token = await TokenManage.getToken();
+    if (token == null || token.isEmpty) return;
+    final res = await handleLogin(token);
+    if (res is String) {
+      await TokenManage.removeToken();
+      await UserManage.removeUserInfo();
+      return;
+    }
+    final userInfo = {
+      'id': res['id'],
+      'loginname': res['loginname'],
+      'avatar_url': res['avatar_url'],
+    };
+    await UserManage.setUserInfo(userInfo);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
